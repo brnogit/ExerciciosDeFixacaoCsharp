2050d2944de6e0646f2366b12431464b439dfff9
bmotadev <bmotadev@gmail.com>
2022-10-05 17:33:34 -0300

    Aula 23 - Exercicio Proposto 1002 URI

---
+++ Aula23ExercicioProposto1002/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace proposto1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 3.14159;
+
+            double raio = double.Parse(Console.ReadLine());
+
+
+            double area = n * (Math.Pow(raio, 2));
+
+            Console.WriteLine($"A={area:F4}");
+
+            Console.ReadLine();
+        }
+    }
+}
