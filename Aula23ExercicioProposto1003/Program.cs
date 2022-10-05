3645a291570de16f23eff802a4fbe2fae622fd7b
bmotadev <bmotadev@gmail.com>
2022-10-05 17:40:11 -0300

    Aula 23 - Exercicio Proposto 1003 URI

---
+++ Aula23ExercicioProposto1003/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercicioproposto1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA = int.Parse(Console.ReadLine());
+            int valorB = int.Parse(Console.ReadLine());
+
+
+            int soma = valorA + valorB;
+
+            Console.WriteLine($"SOMA = {soma}");
+
+            Console.ReadLine();
+        }
+    }
+}
