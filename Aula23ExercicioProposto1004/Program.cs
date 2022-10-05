7f96e1b34dff4f0a731ae050e7921a1a6a7866ee
bmotadev <bmotadev@gmail.com>
2022-10-05 18:02:39 -0300

    Aula 23 - Exercicio Proposto 1004

---
+++ Aula23ExercicioProposto1004/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercicioproposto1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA = int.Parse(Console.ReadLine());
+            int valorB = int.Parse(Console.ReadLine());
+
+            int prod = valorA * valorB;
+
+            Console.WriteLine($"PROD = {prod}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
