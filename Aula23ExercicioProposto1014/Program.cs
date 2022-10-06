6b05a2928fc740b9638336dce76bf2931a7e48c9
bmotadev <bmotadev@gmail.com>
2022-10-06 16:34:57 -0300

    Aula 23 - Exercicio Proposto 1014

---
+++ Aula23ExercicioProposto1014/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto1014
+{
+    class Program
+    {
+        static void Main(string[] argfs)
+        {
+            int distanciaTotalX = int.Parse(Console.ReadLine());
+            double totalDeCombustivelGastoY = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double consumoMedioDoAutomovel = distanciaTotalX / totalDeCombustivelGastoY;
+
+            Console.WriteLine($"{consumoMedioDoAutomovel:F3} km/l", CultureInfo.InvariantCulture);
+        }
+    }
+}
