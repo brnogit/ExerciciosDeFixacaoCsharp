7b32019d770626fd89099d128c9164edabd27978
bmotadev <bmotadev@gmail.com>
2022-10-06 18:39:38 -0300

    Aula 23 - Exercicio Proposto 1006

---
+++ Aula23ExercicioProposto1006/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double valorB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double valorC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = ((valorA * 2) + (valorB * 3) + (valorC * 5)) / 10;
+
+            Console.WriteLine($"MEDIA = {media:F1}", CultureInfo.InvariantCulture);
+        }
+    }
+}
