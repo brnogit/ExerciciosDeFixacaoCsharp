e808d5d6d67538968e496590fcb03331612692d0
bmotadev <bmotadev@gmail.com>
2022-10-06 14:09:07 -0300

    Aula 23 - Exercicio Proposto 1008

---
+++ Aula23ExercicioProposto1008/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroFuncionaro = int.Parse(Console.ReadLine());
+            int horasTrabalhadas = int.Parse(Console.ReadLine());
+            double salarioPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salarioMes = horasTrabalhadas * salarioPorHora;
+
+            Console.WriteLine($"NUMBER = {numeroFuncionaro}");
+            Console.WriteLine($"SALARY = U$ {salarioMes:F2}", CultureInfo.InvariantCulture);
+
+            Console.ReadLine();
+        }
+    }
+}
