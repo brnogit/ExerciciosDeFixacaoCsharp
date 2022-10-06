93a24c3a7fdbbbf48c925cb2016be06811b8147c
bmotadev <bmotadev@gmail.com>
2022-10-06 17:57:43 -0300

    Aula 23 - Exercicio Proposto 1016

---
+++ Aula23ExercicioProposto1016/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercicioproposto1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distanciaEmKm = int.Parse(Console.ReadLine());
+
+            int tempoParaDistanciar = distanciaEmKm * 2;
+
+            Console.WriteLine($"{tempoParaDistanciar} minutos");
+        }
+    }
+}
