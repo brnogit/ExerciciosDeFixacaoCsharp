c7bc04001c6d1f4e71d5aa6e5d5e29514ac1bc00
bmotadev <bmotadev@gmail.com>
2022-10-06 18:17:07 -0300

    Aula 23 - Exercicio Proposto 1017

---
+++ Aula23ExercicioProposto1017/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace exercicioproposto1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double consumoPorLitroKm = 12;
+
+            double tempoGastoNaViagem = double.Parse(Console.ReadLine());
+            double velocidadeMedia = double.Parse(Console.ReadLine());
+
+
+            double litrosNecessarios = (velocidadeMedia / consumoPorLitroKm) * tempoGastoNaViagem;
+
+
+            Console.WriteLine($"{litrosNecessarios:F3}");
+        }
+    }
+}
