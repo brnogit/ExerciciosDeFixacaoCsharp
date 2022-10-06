d16678f7cd2a06a945720acf8343e634ca4eed01
bmotadev <bmotadev@gmail.com>
2022-10-06 18:32:28 -0300

    Aula 23 - Exercicio Proposto 1005

---
+++ Aula23ExercicioProposto1005/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double valorB = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double mediaDoAluno = ((valorA * 3.5) + (valorB * 7.5)) / 11;
+
+            Console.WriteLine($"MEDIA = {mediaDoAluno:F5}", CultureInfo.InvariantCulture);
+        }
+    }
+}
