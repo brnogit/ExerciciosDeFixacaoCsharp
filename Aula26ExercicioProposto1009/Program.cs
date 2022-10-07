2c5eba5d692ae618490d7e6d11757afda846fae7
bmotadev <bmotadev@gmail.com>
2022-10-07 14:23:34 -0300

    Aula 26 - Exercicio Proposto 1009

---
+++ Aula26ExercicioProposto1009/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vendedor = Console.ReadLine();
+            double salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double totalDeVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double comissao = totalDeVendas * (15.0/100.0);
+
+            double totalAReceber = salarioFixo + comissao;
+
+            Console.WriteLine($"TOTAL = {totalAReceber:F2}");
+
+            Console.ReadLine();
+        }
+    }
+}
