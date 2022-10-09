0e393397ac34ae9a809b153bcf231a88c831f254
bmotadev <bmotadev@gmail.com>
2022-10-08 23:10:55 -0300

    Aula 26 - Exercicio Proposto 1015

---
+++ Aula26ExercicioProposto1015/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto1015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
