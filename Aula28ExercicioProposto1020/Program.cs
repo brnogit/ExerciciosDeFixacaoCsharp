b1f93a2f433250f58c22524ebc3f186b03198b20
bmotadev <bmotadev@gmail.com>
2022-10-10 16:41:24 -0300

    aula 28 - exercicio proposto 1020

---
+++ Aula28ExercicioProposto1020/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exercicioproposto1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeAno, idadeMes, idadeDias, restoDaIdade;
+
+            int idadeDaPessoaEmDias = int.Parse(Console.ReadLine());
+
+            idadeAno = idadeDaPessoaEmDias / 365;
+            restoDaIdade = idadeDaPessoaEmDias % 365;
+
+            idadeMes = restoDaIdade / 30;
+            idadeDias = restoDaIdade % 30;
+
+
+            Console.WriteLine($"{idadeAno} ano (s)");
+            Console.WriteLine($"{idadeMes} mes (es)");
+            Console.WriteLine($"{idadeDias} dia (s)");
+        }
+    }
+}
