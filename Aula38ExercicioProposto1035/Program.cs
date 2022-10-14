7921007c1de2b665b18f137f243fd79cd45e1d71
bmotadev <bmotadev@gmail.com>
2022-10-14 13:52:21 -0300

    aula 38 exercicio proposto 1035

---
+++ Aula38ExercicioProposto1035/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercicioproposto1035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA, valorB, valorC, valorD;
+
+
+            string[] valores = Console.ReadLine().Split(' ');
+            valorA = int.Parse(valores[0]);
+            valorB = int.Parse(valores[1]);
+            valorC = int.Parse(valores[2]);
+            valorD = int.Parse(valores[3]);
+
+            if ((valorB > valorC && valorD > valorA) && ((valorC + valorD) > (valorA + valorB)) && (valorC > 0 && valorD > 0) && (valorA % 2 == 0))
+            {
+                Console.WriteLine("Valores aceitos");
+            } 
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+        }
+    }
+}
