c26470cff9455cde142993159338f97839d4d37d
bmotadev <bmotadev@gmail.com>
2022-10-14 15:24:12 -0300

    aula 38 exercicio proposto 1046 tempo de jogo

---
+++ Aula38ExercicioProposto1046/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercicioproposto1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaDeInicio, horaDeFim, diferenca; 
+
+            string[] valores = Console.ReadLine().Split(' ');
+            horaDeInicio = int.Parse(valores[0]);
+            horaDeFim = int.Parse(valores[1]);
+
+            
+
+            if( horaDeInicio >= horaDeFim)
+            {
+                diferenca = 24 - (horaDeInicio - horaDeFim);
+            } else
+            {
+                diferenca = horaDeFim - horaDeInicio;
+            }
+
+            Console.WriteLine($"O JOGO DUROU {diferenca} HORA(S)");
+        }
+    }
+}
