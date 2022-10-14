e44dbfa317c798e783d99580dbd4cd4906ca3097
bmotadev <bmotadev@gmail.com>
2022-10-14 14:51:00 -0300

    aula 38 exercicio proposto 1044 multiplos

---
+++ Aula38ExercicioProposto1044/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercicioproposto1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA, valorB;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            valorA = int.Parse(valores[0]);
+            valorB = int.Parse(valores[1]);
+
+            if(valorA % valorB == 0 || valorB % valorA == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+                
+            } else
+            {
+                
+                Console.WriteLine("Nao sao Multiplos");
+            }
+            
+
+        }
+    }
+}
