2f8242db57756d8cc3be0448f702a476d0e3c288
bmotadev <bmotadev@gmail.com>
2022-10-14 18:34:07 -0300

    aula 38 exercicio proposto 1048

---
+++ Aula38ExercicioProposto1048/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto1048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            double novoSalario, valorDoReajuste, porcentagem;
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if(salario > 0 && salario <= 400.00)
+            {
+                porcentagem = 15;
+                novoSalario = salario + (salario * (porcentagem / 100.0));
+                valorDoReajuste = novoSalario - salario;
+
+                Console.WriteLine($"Novo salario: {novoSalario:F2}");
+                Console.WriteLine($"Reajuste ganho: {valorDoReajuste:F2}");
+                Console.WriteLine($"Em percentual: {porcentagem} %");
+            }
+            if(salario >= 400.01 && salario <= 800.00)
+            {
+                porcentagem = 12;
+                novoSalario = salario + (salario * (porcentagem / 100.0));
+                valorDoReajuste = novoSalario - salario;
+
+                Console.WriteLine($"Novo salario: {novoSalario:F2}");
+                Console.WriteLine($"Reajuste ganho: {valorDoReajuste:F2}");
+                Console.WriteLine($"Em percentual: {porcentagem} %");
+            }
+            if (salario >= 800.01 && salario <= 1200.00)
+            {
+                porcentagem = 10;
+                novoSalario = salario + (salario * (porcentagem / 100.0));
+                valorDoReajuste = novoSalario - salario;
+
+                Console.WriteLine($"Novo salario: {novoSalario:F2}");
+                Console.WriteLine($"Reajuste ganho: {valorDoReajuste:F2}");
+                Console.WriteLine($"Em percentual: {porcentagem} %");
+            }
+            if (salario >= 1200.01 && salario <= 2000.00)
+            {
+                porcentagem = 7;
+                novoSalario = salario + (salario * (porcentagem / 100.0));
+                valorDoReajuste = novoSalario - salario;
+
+                Console.WriteLine($"Novo salario: {novoSalario:F2}");
+                Console.WriteLine($"Reajuste ganho: {valorDoReajuste:F2}");
+                Console.WriteLine($"Em percentual: {porcentagem} %");
+            } else if (salario > 2000.00)
+            {
+                porcentagem = 4;
+                novoSalario = salario + (salario * (porcentagem / 100.0));
+                valorDoReajuste = novoSalario - salario;
+
+                Console.WriteLine($"Novo salario: {novoSalario:F2}");
+                Console.WriteLine($"Reajuste ganho: {valorDoReajuste:F2}");
+                Console.WriteLine($"Em percentual: {porcentagem} %");
+            }
+
+            
+        }
+    }
+}
