f870afd6d26771e9c03379b929cd39536d5bfe95
bmotadev <bmotadev@gmail.com>
2022-10-16 18:11:43 -0300

    Aula 38 exercicio proposto 1040 - concluido

--- Aula38ExercicioProposto1040/Program.cs
+++ Aula38ExercicioProposto1040/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto1040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double notaA, notaB, notaC, notaD, media, notaDoExame;
+
+            string[] notas = Console.ReadLine().Split(' ');
+            notaA = double.Parse(notas[0], CultureInfo.InvariantCulture);
+            notaB = double.Parse(notas[1], CultureInfo.InvariantCulture);
+            notaC = double.Parse(notas[2], CultureInfo.InvariantCulture);
+            notaD = double.Parse(notas[3], CultureInfo.InvariantCulture);
+
+            media = ((notaA * 2) + (notaB * 3) + (notaC * 4) + (notaD * 1)) / 10;
+            media = Math.Truncate(media * 10)/10;
+            Console.WriteLine($"Media: {media:F1}", CultureInfo.InvariantCulture);
+
+            // Se esta média for maior ou igual a 7.0, imprima a mensagem "Aluno aprovado.".
+            if(media >= 7.0)
+            {
+                Console.WriteLine("Aluno aprovado.");
+            }
+            else if(media < 5.0)
+            {
+                Console.WriteLine("Aluno reprovado.");
+            }
+            else if(media >= 5.0 && media <= 6.9)
+            {
+                Console.WriteLine("Aluno em exame.");
+
+                notaDoExame = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine($"Nota do exame: {notaDoExame:F1}", CultureInfo.InvariantCulture);
+
+                media = (media + notaDoExame) / 2;
+
+                if(media >= 5.0)
+                {
+                    Console.WriteLine("Aluno aprovado.");
+                }
+                else if(media <= 4.9)
+                {
+                    Console.WriteLine("Aluno reprovado.");
+                }
+                Console.WriteLine($"Media final: {media:F1}", CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
