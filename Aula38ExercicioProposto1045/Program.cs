4508f3bf37fc8f7832d30975f347e9766c9b529f
bmotadev <bmotadev@gmail.com>
2022-10-17 18:01:20 -0300

    aula 38 exercicio proposto 1045 concluido

---
+++ Aula38ExercicioProposto1045/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto1045
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorA, valorB, valorC, maior, medio,menor;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            valorA = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            valorB = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            valorC = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            if(valorA >= valorB && valorA >= valorC)
+            {
+                maior = valorA;
+                medio = valorB;
+                menor = valorC;
+            }
+            else if(valorB >= valorA && valorB >= valorC)
+            {
+                maior = valorB;
+                medio = valorA;
+                menor = valorC;
+            } else
+            {
+                maior = valorC;
+                medio = valorA;
+                menor = valorB;
+            }
+
+
+
+
+            if(maior >= (medio + menor))
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+            } 
+            else if(Math.Pow(maior,2) == (Math.Pow(medio, 2) + Math.Pow(menor, 2)))
+            {
+                Console.WriteLine("TRIANGULO RETANGULO");
+            }
+            else if(Math.Pow(maior, 2) > (Math.Pow(medio, 2) + Math.Pow(menor, 2)))
+            {
+                Console.WriteLine("TRIANGULO OBTUSANGULO");
+            }
+            else if(Math.Pow(maior, 2) < (Math.Pow(medio, 2) + Math.Pow(menor, 2)))
+            {
+                Console.WriteLine("TRIANGULO ACUTANGULO");
+            }
+
+            if(maior == medio && maior == menor)
+            {
+                Console.WriteLine("TRIANGULO EQUILATERO");
+            } else if ((maior == medio && maior !=  menor) || (maior == menor && maior != medio) || (medio == menor && medio != maior))
+            {
+                Console.WriteLine("TRIANGULO ISOSCELES");
+            }
+        }
+    }
+}
