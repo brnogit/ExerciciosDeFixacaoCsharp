2f7986465ea2c099de37d4c2abc2f283b74f23eb
bmotadev <bmotadev@gmail.com>
2022-10-17 15:50:01 -0300

    aula 38 exercicio proposto 1037 concluido

--- Aula38ExercicioProposto1037/Program.cs
+++ Aula38ExercicioProposto1037/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto1037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorQualquer = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if(valorQualquer >= 25.00001 && valorQualquer <= 50.00000)
+            {
+                Console.WriteLine($"Intervalo (25,50]");
+            }
+            else if (valorQualquer >= 0.0 && valorQualquer <= 25.00000)
+            {
+                Console.WriteLine($"Intervalo [0,25]");
+            }
+            else if(valorQualquer >= 75.00001 && valorQualquer <= 100.00000)
+            {
+                Console.WriteLine($"Intervalo (75,100]");
+            }
+            else
+            {
+                Console.WriteLine($"Fora de intervalo");
+            }
+        }
+    }
+}
