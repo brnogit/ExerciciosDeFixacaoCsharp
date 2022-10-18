6f60c0b1784adbc765c1af900bf67d34af65ec29
bmotadev <bmotadev@gmail.com>
2022-10-18 13:28:28 -0300

    aula 38 exercicio proposto 1047 - concluido

---
+++ Aula38ExercicioProposto1047/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercicioproposto1047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaInicial, minutoInicial, horaFinal, minutoFinal, duracaoDoJogo, tempoInicialEmMinutos, tempoFinalEmMinutos, diferenca, horas, minutos;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            horaInicial = int.Parse(valores[0]);
+            minutoInicial = int.Parse(valores[1]);
+            horaFinal = int.Parse(valores[2]);
+            minutoFinal = int.Parse(valores[3]);
+
+            tempoInicialEmMinutos = (horaInicial * 60) + minutoInicial;
+            tempoFinalEmMinutos = (horaFinal * 60) + minutoFinal;
+
+            
+            if(tempoInicialEmMinutos >= tempoFinalEmMinutos)
+            {
+                diferenca = (24 * 60) - (tempoInicialEmMinutos - tempoFinalEmMinutos);
+            } 
+            else{
+                diferenca = tempoFinalEmMinutos - tempoInicialEmMinutos;    
+            }
+
+            horas = diferenca / 60;
+            minutos = diferenca % 60;
+            Console.WriteLine($"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)");
+        }
+    }
+}
