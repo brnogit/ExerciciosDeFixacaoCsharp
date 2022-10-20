4dca0bbafd8bb105188366f9e859ed3e941e7e3a
bmotadev <bmotadev@gmail.com>
2022-10-20 17:06:58 -0300

    aula 45 exercicio proposto 1151 concluido

---
+++ Aula45ExercicioProposto1115/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace exercicioproposto1115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coodernadaX, coodernadaY;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            coodernadaX = int.Parse(vet[0]);
+            coodernadaY = int.Parse(vet[1]);
+
+            while(coodernadaX != 0 || coodernadaY != 0)
+            {
+                if(coodernadaX > 0 && coodernadaY > 0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                else if (coodernadaY > 0 && coodernadaX < 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+                else if(coodernadaY < 0 && coodernadaX < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+                else if(coodernadaY < 0 && coodernadaX > 0)
+                {
+                    Console.WriteLine("quarto");
+                }
+                vet = Console.ReadLine().Split(' ');
+                coodernadaX = int.Parse(vet[0]);
+                coodernadaY = int.Parse(vet[1]);
+            }
+        }
+    }
+}
