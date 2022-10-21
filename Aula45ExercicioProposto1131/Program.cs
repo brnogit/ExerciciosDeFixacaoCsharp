5e7a619856755e3c72bfcfe041fd17b8bbdbf4bf
bmotadev <bmotadev@gmail.com>
2022-10-21 16:14:29 -0300

    aula 45 exercicio proposto 1131

---
+++ Aula45ExercicioProposto1131/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int golInter, golGremio, grenal;
+
+grenal = 1;
+
+while (grenal == 1)
+{
+    string[] gols = Console.ReadLine().Split(' ');
+    golInter = int.Parse(gols[0]);
+    golGremio = int.Parse(gols[1]);
+
+    Console.WriteLine("Novo grenal (1-sim 2-nao");
+    grenal = int.Parse(Console.ReadLine());
+}
+
+
+
