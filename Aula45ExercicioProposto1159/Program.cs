ff2bfe9c2af30b445fd157ec599b303506659a68
bmotadev <bmotadev@gmail.com>
2022-10-21 12:49:10 -0300

    aula 45 exercicio proposto 1159 concluido

---
+++ Aula45ExercicioProposto1159/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace exercicioproposto1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorX, contador;
+            int[] valores = new int[5];
+
+            valorX = int.Parse(Console.ReadLine());
+
+            while(valorX != 0)
+            {
+                if(valorX % 2 == 0)
+                {
+                    contador = 0;
+                    while (contador < 5)
+                    {
+                        valores[contador] = valorX;
+                        valorX = valorX + 2;
+                        contador++;
+                    }
+                    valorX = valores[0] + valores[1] + valores[2] + valores[3] + valores[4];
+                    Console.WriteLine(valorX);
+
+                }
+                else
+                {
+                    valorX++;
+                    contador = 0;
+                    while (contador < 5)
+                    {
+                        valores[contador] = valorX;
+                        valorX = valorX + 2;
+                        contador++;
+                    }
+                    valorX = valores[0] + valores[1] + valores[2] + valores[3] + valores[4];
+                    Console.WriteLine(valorX);
+
+                }     
+                valorX = int.Parse(Console.ReadLine());
+            }
+            
+
+        }
+    }
+}
