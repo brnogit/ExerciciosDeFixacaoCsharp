bf853b3147ee6f635e7905ddb594bd4b8ff280eb
bmotadev <bmotadev@gmail.com>
2022-10-21 11:35:04 -0300

    aula 45 exercicio proposto 1154 concluido

---
+++ Aula45ExercicioProposto1154/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercicioproposto1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade, contador, idadeTotal;
+            double mediaIdade;
+
+            contador = 0;
+            idadeTotal = 0;
+
+            idade = int.Parse(Console.ReadLine());
+            while(idade >= 0)
+            {
+                idadeTotal = idadeTotal + idade;
+                contador++;
+                idade = int.Parse(Console.ReadLine());
+            }
+            mediaIdade = (double)idadeTotal / contador;
+            Console.WriteLine(mediaIdade.ToString("F2"));
+        }
+    }
+}
