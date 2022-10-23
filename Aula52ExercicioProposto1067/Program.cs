5218322662be6da4d7b98ec3eafe96503e91c50b
bmotadev <bmotadev@gmail.com>
2022-10-23 13:48:54 -0300

    aula 52 exercicio proposto 1067 concluido

---
+++ Aula52ExercicioProposto1067/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicioproposto1067
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorX;
+            valorX = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i <= valorX; i++)
+            {
+                if(i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
