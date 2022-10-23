d8dec64da0fb4b1cea241e26684b9d73f9e358a7
bmotadev <bmotadev@gmail.com>
2022-10-23 14:59:38 -0300

    aula 52 exercicio proposto 1074

---
+++ Aula52ExercicioProposto1074/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace exercicioproposto1074
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorN, valorX;
+            valorN = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < valorN; i++)
+            {
+                valorX = int.Parse(Console.ReadLine());
+                if( valorX % 2 == 0 && valorX > 0)
+                {
+                    Console.WriteLine("EVEN POSITIVE");
+                }
+                else if(valorX % 2 == 1 && valorX > 0)
+                {
+                    Console.WriteLine("ODD POSITIVE");
+                }
+                else if( valorX % 2 == 0 && valorX < 0)
+                {
+                    Console.WriteLine("EVEN NEGATIVE");
+                }
+                else if( valorX % 2 == -1 && valorX < 0)
+                {
+                    Console.WriteLine("ODD NEGATIVE");
+                }
+                else if(valorX == 0)
+                {
+                    Console.WriteLine("NULL");
+                }
+            }
+        }
+    }
+}
