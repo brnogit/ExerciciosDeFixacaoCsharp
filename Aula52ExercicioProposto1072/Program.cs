b975f74c5b539803744a2d68d1e1b8ddf2432291
bmotadev <bmotadev@gmail.com>
2022-10-23 14:16:04 -0300

    aula 52 exercicio proposto 1072

---
+++ Aula52ExercicioProposto1072/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicioproposto1072
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorN, valorX, qntdeDentro, qntdeFora;
+            valorN = int.Parse(Console.ReadLine());
+
+            qntdeFora = 0;
+            qntdeDentro = 0;
+            valorX = 0;
+            for(int i = 0; i < valorN; i++)
+            {
+                valorX = int.Parse(Console.ReadLine());
+                if(valorX >= 10 && valorX <= 20)
+                {
+                    qntdeDentro++;
+                } else
+                {
+                    qntdeFora++;
+                }
+            }
+            Console.WriteLine($"{qntdeDentro} in");
+            Console.WriteLine($"{qntdeFora} out");
+        }
+    }
+}
