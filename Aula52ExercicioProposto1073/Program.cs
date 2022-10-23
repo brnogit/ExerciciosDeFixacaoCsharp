ddbb590824390089f65359e4e83c66ba4c229117
bmotadev <bmotadev@gmail.com>
2022-10-23 14:41:19 -0300

    aula 52 exercicio proposto 1073 concluido

---
+++ Aula52ExercicioProposto1073/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicioproposto1073
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorN;
+            valorN = int.Parse(Console.ReadLine());
+
+                for (int i = 2; i <= valorN; i+=2)
+                {
+                    if(i % 2 == 0)
+                    {
+                    Console.WriteLine(i + "^" + 2 + " = " + Math.Pow(i, 2));
+                    }             
+                }
+        }
+    }
+}
