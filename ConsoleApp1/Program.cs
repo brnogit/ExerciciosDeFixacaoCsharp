1b598d2df0204e6e52516fb59ea92a29a2bc600f
bmotadev <bmotadev@gmail.com>
2022-10-23 15:09:06 -0300

    aula 52 exercicio proposto 1075

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicioproposto1075
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorN;
+            valorN = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                if (i % valorN == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
