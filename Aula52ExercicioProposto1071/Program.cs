e74a374c51333500ff2ed1068ff32a7791752bc1
bmotadev <bmotadev@gmail.com>
2022-10-23 14:03:23 -0300

    aula 52 exercicio proposto 1071

---
+++ Aula52ExercicioProposto1071/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace exercicioproposto1071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorX, valorY, min, max, soma;
+
+            valorX = int.Parse(Console.ReadLine());
+            valorY = int.Parse(Console.ReadLine());
+
+            if(valorX > valorY)
+            {
+                min = valorY;
+                max = valorX;
+            } else
+            {
+                min = valorX;
+                max = valorY;
+            }
+
+            soma = 0;
+            for (int i = min + 1; i < max; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
