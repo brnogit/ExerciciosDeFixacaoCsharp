7bee07b925ba2cb3fb9aaa3c3a28e16259e3a49f
bmotadev <bmotadev@gmail.com>
2022-10-24 13:09:00 -0300

    aula 52 exercicio proposto 1080

--- Aula52ExercicioProposto1080/Program.cs
+++ Aula52ExercicioProposto1080/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+
+namespace exercicioproposto1080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, maiorValor, posicao;
+            valor = 0;
+            maiorValor = 0;
+            posicao = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                valor = int.Parse(Console.ReadLine());
+                if (valor > maiorValor)
+                {
+                    maiorValor = valor;
+                    posicao = i;
+                }
+            }
+            Console.WriteLine(maiorValor);
+            Console.WriteLine(posicao);
+        }
+    }
+}
