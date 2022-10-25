452335987497cdee57049bf6526b8bff4e90d4cb
bmotadev <bmotadev@gmail.com>
2022-10-25 13:55:36 -0300

    aula 61 exercicio proposto 01 - vetores

---
+++ Aula61ExercicioProposto01Vetores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+double numeroMaior, posicao;
+int numero;
+string[] vetorDeNumeros;
+double[] vetorReal;
+
+numero = int.Parse(Console.ReadLine());
+vetorDeNumeros = new string[numero];
+vetorReal = new double[numero];
+
+vetorDeNumeros = Console.ReadLine().Split(' ');
+for(int i = 0; i < numero; i++)
+{
+    vetorReal[i] = double.Parse(vetorDeNumeros[i], CultureInfo.InvariantCulture);
+}
+
+numeroMaior = 0;
+posicao = 0;
+for(int i = 0; i < numero; i++)
+{
+    if (vetorReal[i] > numeroMaior)
+    {
+        numeroMaior = vetorReal[i];
+        posicao = i;
+    }
+}
+
+
+Console.WriteLine(numeroMaior.ToString("F1", CultureInfo.InvariantCulture));
+Console.WriteLine(posicao);
+
