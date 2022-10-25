66aedabd94d8015178b2cda6635f77124dfa61fd
bmotadev <bmotadev@gmail.com>
2022-10-25 17:57:29 -0300

    aula 61 exercicio proposto 02 vetores

---
+++ Aula61ExercicioProposto02Vetoree/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int numeros, qntdPar;
+numeros = int.Parse(Console.ReadLine());
+
+int[] vet = new int[numeros];
+string[] numeroVetor = new string[numeros];
+numeroVetor = Console.ReadLine().Split(' ');
+
+
+// Conveter em vetor de numeros inteiros
+for(int i = 0; i < numeros; i++)
+{
+    vet[i] = int.Parse(numeroVetor[i]);
+}
+
+
+// verifica quantos numeros pares existem e armazena em um novo vetor
+int[] pares = new int[numeros];
+qntdPar = 0;
+for(int i = 0; i < numeros; i++)
+{
+    if (vet[i] % 2 == 0 && vet[i] != 0)
+    {  
+        pares[qntdPar] = vet[i];
+        qntdPar++;
+    }
+    
+}
+
+
+// imprime os numeros
+for (int i = 0; i < qntdPar; i++)
+{
+    Console.Write(pares[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine(qntdPar);
