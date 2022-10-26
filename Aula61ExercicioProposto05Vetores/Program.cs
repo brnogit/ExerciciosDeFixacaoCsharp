2524f4e038d2c55d1cf19213e4306623809ca3f8
bmotadev <bmotadev@gmail.com>
2022-10-26 16:27:27 -0300

    Aula 61 exercicio proposto 05 vetores

---
+++ Aula61ExercicioProposto05Vetores/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+int valorN, qntdPar;
+double media;
+valorN = int.Parse(Console.ReadLine());
+
+string[] vet = new string[valorN];
+vet = Console.ReadLine().Split(' ');
+
+double[] numeros = new double[valorN];
+for( int i = 0; i < valorN; i++)
+{
+    numeros[i] = double.Parse(vet[i], CultureInfo.InvariantCulture);
+}
+
+media = 0;
+qntdPar = 0;
+for(int i = 0; i < valorN; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        media = media + numeros[i];
+        qntdPar++;
+    }
+}
+
+media = media / qntdPar;
+
+Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
