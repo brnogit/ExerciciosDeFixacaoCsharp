999ff01c710c23f818fdcdc0481e9743935d1d70
bmotadev <bmotadev@gmail.com>
2022-10-26 16:55:04 -0300

    aula 61 exercicio proposto 06 vetores

---
+++ Aula61ExercicioProposto06Vetores/Program.cs
@@ -0,0 +1,36 @@
+﻿int valorN, maiorIdade, pessoa;
+valorN = int.Parse(Console.ReadLine());
+
+string[] pessoas = new string[valorN];
+string[] idades = new string[valorN];
+
+// recebendo as informacoes
+for (int i = 0; i < valorN; i++)
+{
+    string[] temporaria = Console.ReadLine().Split(' ');
+    pessoas[i] = temporaria[0];
+    idades[i] = temporaria[1];
+}
+
+
+//convertendo para int
+int[] idadesNumeros = new int[valorN];
+for( int i = 0; i < valorN; i++)
+{
+    idadesNumeros[i] = int.Parse(idades[i]);
+}
+
+
+//verificando maior idade
+maiorIdade = 0;
+pessoa = 0;
+for(int i = 0; i < valorN; i++)
+{
+    if (idadesNumeros[i] > maiorIdade) 
+    { 
+        maiorIdade = idadesNumeros[i];
+        pessoa = i;
+    }
+}
+
+Console.WriteLine($"Pessoa mais velha: {pessoas[pessoa]}");
