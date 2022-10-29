0de0fa26fdfe69c57f0a9251947d6ca5ef6c33ec
bmotadev <bmotadev@gmail.com>
2022-10-29 16:25:10 -0300

    Aula 68 Exercicio Proposto 02 Matrizes

---
+++ Aula68ExercicioProposto02Matrizes/Program.cs
@@ -0,0 +1,28 @@
+﻿//Ler um número N e depois uma matriz quadrada NxN com números inteiros. Depois, mostrar na tela a soma dos
+//elementos de cada linha da matriz.
+
+
+int numeroN, soma;
+numeroN = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[numeroN, numeroN];
+
+for(int linha = 0; linha < numeroN; linha++)
+{
+    string[] vetorLeitor = Console.ReadLine().Split(' ');
+    for(int coluna = 0; coluna < numeroN; coluna++)
+    {
+        matriz[linha, coluna] = int.Parse(vetorLeitor[coluna]);
+    }
+}
+
+for(int i = 0; i < numeroN; i++)
+{
+    soma = 0;
+    for(int j = 0; j < numeroN; j++)
+    {
+        soma = soma + matriz[i, j];
+        
+    }
+    Console.WriteLine(soma);
+}
