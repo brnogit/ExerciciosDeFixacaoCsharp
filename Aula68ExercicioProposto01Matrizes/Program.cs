c9cfec13094bc5baf7c9595adcd757b8ebb0ce84
bmotadev <bmotadev@gmail.com>
2022-10-29 16:06:14 -0300

    Aula 68 Exercicio Proposto 01 Matrizes

---
+++ Aula68ExercicioProposto01Matrizes/Program.cs
@@ -0,0 +1,32 @@
+﻿
+#region Leitura dos dados
+int linhaN, colunaM;
+string[] vetorLinhaColuna = Console.ReadLine().Split(' ');
+linhaN = int.Parse(vetorLinhaColuna[0]);
+colunaM = int.Parse(vetorLinhaColuna[1]);
+
+int[,] matriz = new int[linhaN, colunaM];
+
+for(int i = 0; i < linhaN; i++)
+{
+    string[] vetorLeitor = Console.ReadLine().Split(' ');
+    for(int j = 0; j < colunaM; j++)
+    {
+        matriz[i, j] = int.Parse(vetorLeitor[j]);
+    }
+}
+#endregion
+
+#region Valores Negativos
+Console.WriteLine("VALORES NEGATIVOS:");
+for (int i = 0; i < linhaN; i++)
+{
+    for(int j = 0; j< colunaM; j++)
+    {
+        if (matriz[i, j] < 0)
+        {
+            Console.WriteLine(matriz[i, j]);
+        }
+    }
+}
+#endregion
