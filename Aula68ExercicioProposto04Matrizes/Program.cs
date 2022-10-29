9e6e26608668b1023814f5da77b6dc07e32ff1ff
bmotadev <bmotadev@gmail.com>
2022-10-29 17:28:11 -0300

    aula 68 exercicio proposto 04 matrizes

---
+++ Aula68ExercicioProposto04Matrizes/Program.cs
@@ -0,0 +1,30 @@
+﻿//4) Ler um inteiro N e uma matriz quadrada de ordem N. Mostrar a soma dos elementos acima da diagonal principal.
+
+
+int numeroN, soma;
+numeroN = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[numeroN, numeroN];
+
+for (int i = 0; i < numeroN; i++)
+{
+    string[] vetorLeitor = Console.ReadLine().Split(' ');
+    for(int j = 0; j < numeroN; j++)
+    {
+        matriz[i, j] = int.Parse(vetorLeitor[j]);
+    }
+}
+
+soma = 0;
+for (int i = 0; i < numeroN; i++)
+{
+    for(int j = 0; j< numeroN; j++)
+    {
+        if(i < j)
+        {
+            soma = soma + matriz[i, j];
+        }
+    }
+}
+
+Console.WriteLine(soma);
