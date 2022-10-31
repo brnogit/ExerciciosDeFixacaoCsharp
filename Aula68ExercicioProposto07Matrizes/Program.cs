425316734c5b0bacbb54d7dcf9527ed7b3631d6d
bmotadev <bmotadev@gmail.com>
2022-10-31 12:45:00 -0300

    aula 68 exercicio proposto 07 matrizes

---
+++ Aula68ExercicioProposto07Matrizes/Program.cs
@@ -0,0 +1,34 @@
+﻿int numeroA, numeroB, fila, ultimoDaFila;
+numeroA = int.Parse(Console.ReadLine());
+numeroB = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[numeroA, numeroB];
+
+for(int i = 0; i < numeroA; i++)
+{
+    string[] vet = Console.ReadLine().Split(' ');
+    for(int j = 0; j < numeroB; j++)
+    {
+        matriz[i, j] = int.Parse(vet[j]);
+    }
+}
+
+fila = int.Parse(Console.ReadLine());
+
+ultimoDaFila = matriz[fila, numeroB - 1];
+
+for(int j = numeroB - 1; j >0; j--)
+{
+    matriz[fila, j] = matriz[fila, j - 1];
+}
+
+matriz[fila, 0] = ultimoDaFila;
+
+for(int i = 0; i < numeroA; i++)
+{
+    for(int j = 0; j < numeroB; j++)
+    {
+        Console.Write(matriz[i, j] + " ");
+    }
+    Console.WriteLine();
+}
