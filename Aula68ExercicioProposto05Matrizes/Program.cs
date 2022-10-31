735070dd9f046b073906691cae216f0af9c5db2e
bmotadev <bmotadev@gmail.com>
2022-10-31 10:50:17 -0300

    aula 68 exercicio proposto 05 matrizes - concluido

--- Aula68ExercicioProposto05Matrizes/Program.cs
+++ Aula68ExercicioProposto05Matrizes/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int linhasM, colunasN;
+
+string[] vetorLeitor = Console.ReadLine().Split(' ');
+linhasM = int.Parse(vetorLeitor[0]);
+colunasN = int.Parse(vetorLeitor[1]);
+
+int[,] matrizA = new int[linhasM, colunasN];
+int[,] matrizB = new int[linhasM, colunasN];
+int[,] matrizC = new int[linhasM, colunasN];
+
+for(int i = 0; i < linhasM; i++)
+{
+    string[] vet = Console.ReadLine().Split(' ');
+    for(int j = 0; j < colunasN; j++)
+    {
+        matrizA[i, j] = int.Parse(vet[j]);
+    }
+}
+
+for(int i = 0; i < linhasM; i++)
+{
+    string[] vet = Console.ReadLine().Split(' ');
+    for(int j = 0; j < colunasN; j++)
+    {
+        matrizB[i, j] = int.Parse(vet[j]);
+    }
+}
+
+for(int i = 0; i < linhasM; i++)
+{
+    for(int j = 0; j < colunasN; j++)
+    {
+        matrizC[i, j] = matrizA[i, j] + matrizB[i, j];
+    }
+}
+
+for(int i = 0; i < linhasM; i ++)
+{
+    Console.WriteLine();
+    for(int j = 0; j < colunasN; j++)
+    {
+        Console.Write(matrizC[i, j] + " ");
+    }
+}
